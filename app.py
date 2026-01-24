6fdbf20f0ff7e05a4b78c1935bd9e149abbbae03
Marijo Erenda <Erenda.Marijo@web.de>
2026-01-24 16:58:06 +0100

    Refactor: project structure

--- app.py
+++ app.py
@@ -18,8 +18,4 @@ def create_app():
 # ===============================
 if __name__ == "__main__":
     app = create_app()
-    app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=3001
-    )
+    app.run(debug=True, host="127.0.0.1", port=3001)
